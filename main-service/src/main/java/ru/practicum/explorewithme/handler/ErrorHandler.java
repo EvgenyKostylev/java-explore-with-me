801e41d9f816e3263d48ba534218aade7b9e4742
Eugeny Kostylev <HasRButNoC@yandex.ru>
2026-03-11 20:33:50 +0300

    Добавлено логирование ошибки отсутствия необходимого параметра строки

--- main-service/src/main/java/ru/practicum/explorewithme/handler/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/explorewithme/handler/ErrorHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -29,6 +30,21 @@ public class ErrorHandler {
                 .timestamp(LocalDateTime.now()).build();
 
         log.warn(error.toString());
+
+        return error;
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingParams(MissingServletRequestParameterException ex) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .reason("Incorrectly made request.")
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        log.warn(error.getMessage());
 
         return error;
     }
